3c4c0b055787e34e23819225ce098addc899379f
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-02-09 11:31:49 +0000

    completed ex1 and ex2

---
+++ ex2.rb
@@ -0,0 +1,10 @@
+# A comment is used to add extra meaning to your code.
+# Anuything after the # symbol is ignored by the interpreter.
+
+puts "If I write something like this" # this portion of text is ignored.
+
+# comments are useful if you want to temporarily disable code but don't want to delete it.
+
+# puts "This won't run"
+
+puts "but this will"
