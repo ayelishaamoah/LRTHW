54978702112d06a8098efa73b2d0aba94bf0049c
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-02-27 00:30:48 +0000

    exercise 5 study drills

---
+++ ex5.rb
@@ -0,0 +1,14 @@
+#Exercise 5: More Variables and Printing
+name = "Zed A. Shaw"
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talke about #{name}"
+
+height_in_cm = height * 2.54
+
+puts "my height in cm is: #{height_in_cm} cm"
